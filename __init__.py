de25ca52476a6fc68ed3e0136b11ef588b3cf8a5
goldyfruit <gaetan.trellu@ibm.com>
2020-12-09 15:03:43 -0500

    [init] List music services

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_handler
 from soco import discover
+from soco.music_services import MusicService
 
 
 class SonosController(MycroftSkill):
@@ -11,11 +12,11 @@ class SonosController(MycroftSkill):
         self.speakers = discover()
         if not self.speakers:
             self.log.error(
-                'I could not find any Sonos devices on your network')
+                'I could not find any devices on your network')
             self.speak_dialog('error.disovery')
         else:
             self.log.info(
-                '{} Sonos device(s) found'.format(len(self.speakers)))
+                '{} device(s) found'.format(len(self.speakers)))
 
     @intent_handler('sonos.discovery.intent')
     def handle_speaker_discovery(self, message):
@@ -23,14 +24,28 @@ class SonosController(MycroftSkill):
         if self.speakers:
             self.speak_dialog('sonos.discovery', data={
                               "total": len(self.speakers)})
-            list_devices = self.ask_yesno('sonos.list')
-            if list_devices == 'yes':
+            list_device = self.ask_yesno('sonos.list')
+            if list_device == 'yes':
                 for speaker in self.speakers:
                     self.speak(speaker.player_name)
+
+    @intent_handler('sonos.service.intent')
+    def handle_subscribed_services(self, message):
+        # Commented until SoCo integrates this method back
+        # services = MusicService.get_subscribed_services_names()
+        services = ['spotify', 'amazon music']
+        if services:
+            self.speak_dialog('sonos.service', data={
+                              "total": len(services)})
+            list_service = self.ask_yesno('sonos.list')
+            if list_service == 'yes':
+                for service in services:
+                    self.speak(service)
 
     def initialize(self):
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
+        self._discovery()
 
     def on_settings_changed(self):
         return
