f7834c59afdcb33da043c8e510238e9198e582e2
goldyfruit <gaetan.trellu@ibm.com>
2020-12-09 21:21:20 -0500

    [global] Big chunk of code added

--- __init__.py
+++ __init__.py
@@ -1,22 +1,53 @@
 from mycroft import MycroftSkill, intent_handler
 from soco import discover
 from soco.music_services import MusicService
+from soco.discovery import by_name
+from soco import exceptions
+from random import choice
 
 
 class SonosController(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.speakers = []
+        self.services = []
+        self.service = 'Amazon Music'
+        self.provider = MusicService(self.service)
 
     def _discovery(self):
-        self.speakers = discover()
+        try:
+            self.speakers = discover()
+        except exceptions.SoCoException as e:
+            self.log.error(e)
+
         if not self.speakers:
-            self.log.error(
+            self.log.warning(
                 'I could not find any devices on your network')
             self.speak_dialog('error.disovery')
         else:
             self.log.info(
                 '{} device(s) found'.format(len(self.speakers)))
+
+    def _subscribed_services(self):
+        try:
+            # Commented until SoCo integrates this method back
+            # self.services = MusicService.get_subscribed_services_names()
+            self.services = ['Spotify', 'Amazon Music']
+            return self.services
+        except exceptions.SoCoException as e:
+            self.log.error(e)
+
+    def _check_category(self, category):
+        for categories in self.provider.available_search_categories:
+            if category in categories:
+                return True
+            return False
+
+    def _check_speaker(self, speaker):
+        for device in self.speakers:
+            if speaker in device.player_name:
+                return True
+            return False
 
     @intent_handler('sonos.discovery.intent')
     def handle_speaker_discovery(self, message):
@@ -31,24 +62,49 @@ class SonosController(MycroftSkill):
 
     @intent_handler('sonos.service.intent')
     def handle_subscribed_services(self, message):
-        # Commented until SoCo integrates this method back
-        # services = MusicService.get_subscribed_services_names()
-        services = ['spotify', 'amazon music']
-        if services:
+        if self.services:
             self.speak_dialog('sonos.service', data={
-                              "total": len(services)})
+                              "total": len(self.services)})
             list_service = self.ask_yesno('sonos.list')
             if list_service == 'yes':
-                for service in services:
+                for service in self.services:
                     self.speak(service)
+            return self.services
         else:
-            self.log.warning('no subscription found to any music service')
+            self.log.warning('no subscription found for any music service')
             self.speak_dialog('error.list')
+
+    @intent_handler('sonos.playlist.intent')
+    def handle_playlist(self, message):
+        service = message.data.get('service')
+        playlist = message.data.get('playlist')
+        speaker = message.data.get('speaker')
+
+        if self.services and service in self.services:
+            if self._check_speaker(speaker):
+                if self._check_category('playlist'):
+                    try:
+                        playlists = service.search('playlists', playlist)
+                        device = by_name(speaker)
+                        device.add_to_queue(choice(playlists))
+                        device.play()
+                    except exceptions.SoCoException as e:
+                        self.log.error(e)
+                else:
+                    self.log.warning(
+                        'there is no playlist category for this service')
+                    self.speak_dialog('error.playlist')
+            else:
+                self.log.warning(
+                    '{} speaker not found'.format(speaker))
+                self.speak_dialog('error.speaker', data={
+                    "speaker": speaker})
 
     def initialize(self):
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
         self._discovery()
+        self._subscribed_services()
 
     def on_settings_changed(self):
         return
