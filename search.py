90107070eecc85fdb741e6a9e260729b1a551a77
goldyfruit <gaetan.trellu@ibm.com>
2021-03-09 17:59:16 -0500

    [search] Add comment and missing try/except

--- search.py
+++ search.py
@@ -6,13 +6,34 @@ from .utils import get_category, check_speaker
 
 def search(self, service, speaker, category, playlist=None, album=None,
            artist=None, track=None):
+    """Search an item to play, it could be a playlist, an album or a track.
+
+    This function will build a dict with all the required information
+    collected and will pass it to the others functions.
+
+    :param service: Music service to search in
+    :type service: string
+    :param speaker: Speaker where to play
+    :type speaker: string
+    :param category: Which category to look into
+    :type category: string
+    :param playlist: Playlist name
+    :type playlist: string, optional
+    :param album: Album name
+    :type album: string, optional
+    :param artist: Artist name
+    :type artist: string, optional
+    :param track: Track name
+    :type track: string, optional
+    :return:
+    :raises SoCoException: Raise SoCoException
+    """
     if service in map(str.lower, set(self.services)):
         device_name = check_speaker(self, speaker)
-
         if device_name:
             provider = get_category(self, service.title(), category)
             if provider:
-                # Build data dictionnary
+                # Build data dictionnary to pass to search_type()
                 data = {}
                 data['service'] = service
                 data['speaker'] = device_name
@@ -29,6 +50,12 @@ def search(self, service, speaker, category, playlist=None, album=None,
 
 
 def search_type(self, data):
+    """This is a meta function that will act as proxy to redirect the
+    query to the right function.
+
+    :param data: Dict with all the required data 
+    :type dict: string
+    """
     if data['category'] == 'playlists':
         search_playlist(self, data)
     # elif data['category'] == 'albums':
@@ -37,42 +64,47 @@ def search_type(self, data):
 
 
 def search_playlist(self, data):
-    # try:
-    picked = None
-    title = None
+    """Search for playlist into Music Library and Music Services.
 
-    device = by_name(data['speaker'])
-    device.clear_queue()
+    :param data: Dict with all the required data
+    :type dict: string
+    :return:
+    :raises SoCoException: Raise SoCoException
+    """
+    try:
+        picked = None
+        title = None
 
-    if data['service'] == 'music library':
+        # Clear the current content playing
+        device = by_name(data['speaker'])
+        device.clear_queue()
 
-        playlists = {}
-        for playlist in data['provider'].get_playlists(
-                search_term=data['playlist'],
-                complete_result=True):
-            playlists[playlist.to_dict()['title']] = playlist.to_dict()[
-                'resources'][0]['uri']
-        if playlists:
-            picked = choice(list(playlists.keys()))
-            device.add_uri_to_queue(playlists[picked])
-            title = picked
+        if data['service'] == 'music library':
+            playlists = {}
+            for playlist in data['provider'].get_playlists(
+                    search_term=data['playlist'],
+                    complete_result=True):
+                playlists[playlist.to_dict()['title']] = playlist.to_dict()[
+                    'resources'][0]['uri']
+            if playlists:
+                picked = choice(list(playlists.keys()))
+                device.add_uri_to_queue(playlists[picked])
+                title = picked
+            else:
+                self.speak_dialog('error.playlist', data={
+                    'playlist': data['playlist']})
+                return
         else:
-            self.speak_dialog('error.playlist', data={
-                'playlist': data['playlist']})
-            return
-    else:
-        playlists = data['provider'].search('playlists', data['playlist'])
-        picked = choice(playlists)
-        device.add_to_queue(picked)
-        title = picked.title
+            playlists = data['provider'].search('playlists', data['playlist'])
+            picked = choice(playlists)
+            device.add_to_queue(picked)
+            title = picked.title
 
-    device.play_from_queue(0)
+        # Play the picked playlist
+        device.play_from_queue(0)
 
-    self.log.debug(
-        '{} playlist from {} on {} started'.format(
-            picked, data['service'], data['speaker']))
-    self.speak_dialog('sonos.playlist', data={
-        'playlist': title, 'service': data['service'],
-        'speaker': data['speaker']})
-    # except exceptions.SoCoException as e:
-    #     self.log.error(e)
+        self.speak_dialog('sonos.playlist', data={
+            'playlist': title, 'service': data['service'],
+            'speaker': data['speaker']})
+    except exceptions.SoCoException as err:
+        self.log.error(err)
