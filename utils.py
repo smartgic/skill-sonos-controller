aba6946ea96823fddf86e08aacbe91aa227ddcc9
goldyfruit <gaetan.trellu@ibm.com>
2021-03-12 18:46:55 -0500

    [utils] Fix variable name

--- utils.py
+++ utils.py
@@ -227,8 +227,8 @@ def run_command(self, command, speaker, state='playing', extras=None):
         elif command == 'get-track':
             _get_track(self, speaker)
         else:
-            for speaker in self.speakers:
-                device_name = check_speaker(self, speaker)
+            for device in self.speakers:
+                device_name = check_speaker(self, device)
                 if get_state(self, device_name) == state.upper():
                     if command in ('vol-up', 'vol-down'):
                         _volume(self, command, device, extras)
