3fa8ca4d86f7cc56f591055a292bbf6fe218a812
Gaëtan Trellu <gaetan.trellu@gmail.com>
2021-08-11 15:43:31 -0400

    [init] Change log_level for SoCo

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 """Sonos controller entrypoint skill
 """
+import logging
 from mycroft import MycroftSkill, intent_handler
+from mycroft.configuration import Configuration
 from .utils import authentication, discovery, subscribed_services, \
     check_service, run_command, get_track
 from .search import search
@@ -23,6 +25,11 @@ class SonosController(MycroftSkill):
         self.settings_change_callback = None
         self.code = None
         self.duck = None
+
+        # Override SoCo logging level to match Mycroft configuration
+        config = Configuration.get(remote=False)
+        logger = logging.getLogger(__name__)
+        logger.setLevel(config.get('log_level', 'INFO'))
 
     def _setup(self):
         """Provision initialized variables and retrieve configuration
