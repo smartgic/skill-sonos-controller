a634a5a828d8d62a4e50fdf84c26b0acbb06b128
Gaëtan Trellu <gaetan.trellu@gmail.com>
2021-08-11 16:24:47 -0400

    [init] Remove logging.basicConfig()

--- __init__.py
+++ __init__.py
@@ -29,7 +29,6 @@ class SonosController(MycroftSkill):
 
         # Override SoCo logging level to match Mycroft configuration
         config = Configuration.get(remote=False)
-        logging.basicConfig()
         logging.getLogger('soco').setLevel(logging.INFO)
 
     def _setup(self):
